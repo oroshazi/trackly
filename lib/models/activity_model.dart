d469e1a2fc25eb49852fda66e188816b93bb22a9
peter <p.oroshazi@gmail.com>
2020-01-25 17:31:34 +0100

    Add SQLite Database helper class Extend activity model with FromJSON function Write activity to database when finished with activity Read activities from database when on timer screen for finsished activities

--- lib/models/activity_model.dart
+++ lib/models/activity_model.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Activity {
+  String id;
   String category;
   String subCategory;
   String duration;
   String notes;
+  String time;
+  String date;
 
   Activity(
-      {@required this.category,
+      {@required this.id,
+      @required this.category,
       @required this.duration,
+      @required this.date,
+      @required this.time,
       this.subCategory,
       this.notes});
+
+  Activity.fromJSON({Map<String, dynamic> json}) {
+    this.id = json["_id"].toString();
+    this.category = json["category"].toString();
+    this.subCategory = json["subCategory"];
+    this.duration = json["duration"];
+    this.notes = json["notes"];
+    this.date = json["date"];
+    this.time = json["time"];
+  }
 }
--- lib/provider/activity_provider.dart
+++ lib/provider/activity_provider.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:trackly_app/helpers/database_helper.dart';
 import 'package:trackly_app/models/activity_model.dart';
 
 class ActivityProvider extends ChangeNotifier {
+  final _dbHelper = DatabaseHelper.instance;
+
   List<Activity> _finishedActivites = [];
   String _selectedActivity;
 
-  get finishedActivities {
+  get finishedActivities  {
+     _queryActivites();
+    // if (finishedActivities.lenght > 0) return activityList;
     return _finishedActivites;
   }
 
@@ -23,9 +28,45 @@ class ActivityProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void finishActivity(Activity activity) {
+  void finishActivity(Activity activity) async {
+    var queryResult;
     // Insert finished activity to the first place of the list
     _finishedActivites.insert(0, activity);
+
+    try {
+      queryResult = await _dbHelper.insert({
+        DatabaseHelper.currentDate: DateTime.now().toString(),
+        DatabaseHelper.currentTime: DateTime.now().toString(),
+        DatabaseHelper.category: activity.category,
+        DatabaseHelper.duration: activity.duration,
+        DatabaseHelper.notes: activity.notes,
+        DatabaseHelper.subCategory: activity.subCategory,
+      });
+      if (queryResult == null) print("error, while inserting.");
+    } catch (e) {
+      print(e);
+      throw Error;
+    }
+
     notifyListeners();
+  }
+
+  void _queryActivites() async {
+    // List<Map<String, dynamic>> allRows;
+    // List<Activity> activityList = [];
+    // try {
+    //   allRows = await _dbHelper.queryAllRows();
+    //   for (var i = 0; i < allRows.length; i++) {
+    //     activityList.add(Activity.fromJSON(json: allRows[i]));
+    //   }
+    //   print("allRows");
+    //   print(allRows);
+    //   _finishedActivites = activityList;
+    //   // notifyListeners();  
+    // } catch (e) {
+    //   print(e); 
+    //   throw Error;
+    // }
+    // return activityList;
   }
 }
